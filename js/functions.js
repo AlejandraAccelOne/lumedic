a1c4a62179ac09aa9c909ab5296da287a8a580ae
Diego Rodriguez <diegorodriguez.tandil@gmail.com>
2020-06-18 17:17:35 -0300

    Scrollmagic on phone image

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,14 @@
-  $(".text-rotater").textrotator({
-    animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
-    separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-    speed: 2000 // How many milliseconds until the next word show.
-  });
+  // $(".text-rotater").textrotator({
+  //   animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+  //   separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+  //   speed: 2000 // How many milliseconds until the next word show.
+  // });
 
-  $(".rotate").textrotator({
-    animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
-    separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-    speed: 2000 // How many milliseconds until the next word show.
-  });
+  // $(".rotate").textrotator({
+  //   animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+  //   separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+  //   speed: 2000 // How many milliseconds until the next word show.
+  // });
 
   $('.carousel').slick({
     slidesToShow: 3,
@@ -52,17 +52,49 @@
   });
   
 
-  
-//
-// ScrollMagic
-//
-  	// init controller
-    var controller = new ScrollMagic.Controller();
+$(document).ready(function(){
 
-    // build scene
-    var scene = new ScrollMagic.Scene(
-        {triggerElement: "#lumedic-phone"})
-        .setClassToggle('#lumedic-phone .innerText','white')
-        .addTo(controller);
+  var texts = [
+    'Clinical Guidance',
+    'Integrated Solution Management',
+    'System Integration & Engineering',
+    'Lumedic.ID Technology Platform'
+  ];
+
+  var controller = new ScrollMagic.Controller();
+  var $text = $('#mainTitle');
+  var textChange = new TimelineMax()
+    .to(".text-loader", 1, {
+        rotation:'0',
+        onStart: function () {$text.html(texts[0]);},
+        onReverseComplete: function () {$text.html(texts[3]);}
+      }
+    )
+    .to(".text-loader", 1, {rotation:'90',
+        onStart: function () {$text.html(texts[1]);},
+        onReverseComplete: function () {$text.html(texts[0]);}
+      }
+    )
+    .to(".text-loader", 1, {rotation:'180',
+        onStart: function () {$text.html(texts[2]);},
+        onReverseComplete: function () {$text.html(texts[1]);}
+      }
+    )    
+    .to(".text-loader", 1, {rotation:'270',
+        onStart: function () {$text.html(texts[3]);},
+        onReverseComplete: function () {$text.html(texts[2]);}
+      }
+    );  
+
+  // build scene
+
+	var textScroll = new ScrollMagic.Scene({
+		triggerElement:'#lumedic-phone',
+		triggerHook:0,
+		duration:$('#lumedic-phone').height()
+	})
+	.setTween(textChange)
+  .setPin('#lumedic-phone')
+  .addTo(controller);
   
-  
+});
