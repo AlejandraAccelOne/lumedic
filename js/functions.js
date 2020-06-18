adefba2cff39d27be01ab4a40f6557516ed1c1f7
alejandra <alejandra@qwavee.com>
2020-06-18 15:52:56 -0300

    Slider on mobile

--- js/functions.js
+++ js/functions.js
@@ -17,27 +17,23 @@
     autoplaySpeed: 2000,
     arrows: false,
     dots: false,
-  //  autoplay: true,
-  //  autoplaySpeed: 1000,
-  //  speed: 2000,
-    //slidesToShow: 4,
-    //slidesToScroll: 1,
-    responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3
-      }
-    }, {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 3
-      }
-    }, {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2
-      }
-    }]
+
+    // responsive: [{
+    //   breakpoint: 1024,
+    //   settings: {
+    //     slidesToShow: 3
+    //   }
+    // }, {
+    //   breakpoint: 600,
+    //   settings: {
+    //     slidesToShow: 3
+    //   }
+    // }, {
+    //   breakpoint: 480,
+    //   settings: {
+    //     slidesToShow: 2
+    //   }
+    // }]
   });
   
   $('.slider-nav').slick({
