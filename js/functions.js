6003b77f77c269e18d51c45a803100f5e95582eb
Diego Rodriguez <diegorodriguez.tandil@gmail.com>
2020-06-18 13:10:14 -0300

    Nav for slider

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-$(".text-rotater").textrotator({
+  $(".text-rotater").textrotator({
     animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
     separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
     speed: 2000 // How many milliseconds until the next word show.
@@ -15,20 +15,13 @@ $(".text-rotater").textrotator({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-
     arrows: false,
+    dots: false,
   //  autoplay: true,
   //  autoplaySpeed: 1000,
   //  speed: 2000,
     //slidesToShow: 4,
     //slidesToScroll: 1,
-    dots: true,
-    customPaging : function(slider, i) {
-      var thumb = $(slider.$slides[i]).data();
-      return '<a class="dot">test'+i+'</a>';
-      },
-
-
     responsive: [{
       breakpoint: 1024,
       settings: {
@@ -45,6 +38,17 @@ $(".text-rotater").textrotator({
         slidesToShow: 2
       }
     }]
+  });
+  
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: false,
+    asNavFor: '.carousel',
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
   });
   
   
