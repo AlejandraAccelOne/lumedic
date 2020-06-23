0f1400ad1dab834497bf506473d0101716bfe77d
alejandra <alejandra@qwavee.com>
2020-06-23 10:04:32 -0300

    Last changes

--- js/functions.js
+++ js/functions.js
@@ -15,10 +15,18 @@
     slidesToScroll: 1,
   //  autoplay: true,
     autoplaySpeed: 2000,
+    initialSlide: 1,
     arrows: false,
     dots: false,
     centerMode: true,
-  });
+    loop: false,
+    infinite: false
+  })
+  // .on('setPosition', function (event, slick) { 
+  //   $('.carousel').$slides.css('height', $('.carousel').$slideTrack.height() + 'px');
+  // })
+  ;
+
   
   $('.slider-nav').slick({
     slidesToShow: 4,
@@ -32,6 +40,41 @@
   //,  variableWidth: true
   });
   
+
+//when the slick slide initializes we want to set all of our slides to the same height
+$('.carousel').on('setPosition', function () {
+	jbResizeSlider();
+});
+ 
+//we need to maintain a set height when a resize event occurs.
+//Some events will through a resize trigger: $(window).trigger('resize');
+$(window).on('resize', function(e) {
+	jbResizeSlider();
+});
+ 
+//since multiple events can trigger a slider adjustment, we will control that adjustment here
+function jbResizeSlider(){
+	$slickSlider = $('.carousel');
+	$slickSlider.find('.slick-slide').height('auto');
+ 
+	var slickTrack = $slickSlider.find('.slick-track');
+  var slickTrackHeight = $(slickTrack).height()-50;
+ 
+	$slickSlider.find('.slick-slide').css('height', slickTrackHeight + 'px');
+}
+
+  // $('.slider').slick({
+  //   autoplay: false,
+  //   dots: false,
+  //   infinite: false,
+  //   arrows: false,
+  //   slidesToShow: 2,
+  //   slidesToScroll: 2,
+  //   rows: 0
+  // })
+  // .on('setPosition', function (event, slick) {
+  //   slick.$slides.css('height', slick.$slideTrack.height() + 'px');
+  // });
 
 $(document).ready(function(){
 
